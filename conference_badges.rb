83a83864caaee5936a7a22e341f0da29d98ee92a
James Batsell <jamesbatsell05@gmail.com>
2019-09-18 00:54:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |element|
+    new_array.push ("Hello, my name is " + ele,ment = "."
+  
+  
